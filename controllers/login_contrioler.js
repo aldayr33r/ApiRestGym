cabe0f36312dafd8405a1c618232b4b771443444
robert <2137000099@utna.edu.mx>
2024-07-24 00:04:33 -0600

    merge del controller registroAltas

--- controllers/login_contrioler.js
+++ controllers/login_contrioler.js
@@ -83,31 +83,6 @@ const registroAltas = async (req, res, next) => {
 };
 
 
-const login = async (req, res) => {
-    const { user, pass } = req.body;
-    const usuario = await usuarios.findOne({ where: { user } });
-    
-    if (!user || !(await user.comparePassword(password))) {
-        return res.status(401).json({ message: 'Invalid credentials' });
-    }
-   
-    res.json({ message: 'Login successful' });
-}
-const logout = async (req, res) => {
-    req.session.destroy(err => {
-        if (err) {
-            return res.status(500).json({ message: 'Error logging out' });
-        }
-        res.json({ message: 'Logout successful' });
-    });
-};
-
-/* const register = async (req, res) => {
-    const { user, pass } = req.body;
-    const usuario = await usuarios.create({ user, pass });
-    res.status(201).json({ message: 'User registered successfully' });
-}; */
-
 module.exports ={
     login, logout,registroAltas
 }
