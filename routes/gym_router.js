45f3017cdc020edbd82a3341463be3a2bb0eb2e5
aldayr33r <aldayrramos5@gmail.com>
2024-08-13 00:37:13 -0600

    se agrego la verficacion del token a las rutas

--- routes/gym_router.js
+++ routes/gym_router.js
@@ -6,16 +6,16 @@ const dietaController= require('../controllers/dieta_controller');
 const verifyToken = require('../controllers/verifyToken')
 
 router.post('/login', loginControl.login);
-router.post('/registro/:user_admin', loginControl.registroAltas);
-router.get('/rutina/:user', SusController.rutina);
-router.get('/perfil/:user', SusController.listar_info_Usuarios);
-router.get('/logs/:user_admin', SusController.listar_logs);
-router.get('/listar_Usuarios/:user_admin', SusController.listar_allUsuarios);
-router.delete('/eliminar_Usuarios/:user/:user_admin', SusController.eliminar_Usuarios);
-router.put('/actualizar/:user/:user_admin', SusController.update_User);
-router.put('/actualizar_info/:user', SusController.update_Info_User);
-router.put('/actualizar_sus/:user/:user_admin', SusController.actualizar_suscripcion);
-router.get('/dieta/:user', dietaController.SendDieta);
+router.post('/registro/:user_admin', verifyToken, loginControl.registroAltas);
+router.get('/rutina/:user', verifyToken, SusController.rutina);
+router.get('/perfil/:user', verifyToken, SusController.listar_info_Usuarios);
+router.get('/logs/:user_admin', verifyToken, SusController.listar_logs);
+router.get('/listar_Usuarios/:user_admin', verifyToken, SusController.listar_allUsuarios);
+router.delete('/eliminar_Usuarios/:user/:user_admin', verifyToken, SusController.eliminar_Usuarios);
+router.put('/actualizar/:user/:user_admin', verifyToken, SusController.update_User);
+router.put('/actualizar_info/:user', verifyToken, SusController.update_Info_User);
+router.put('/actualizar_sus/:user/:user_admin', verifyToken, SusController.actualizar_suscripcion);
+router.get('/dieta/:user', verifyToken, dietaController.SendDieta);
 
 
 
